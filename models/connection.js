8356b237f764d5a11a6883bf53ff950611857280
vivek <vivekkohli935@gmail.com>
2023-09-17 17:27:27 +0530

    Deploy 3

--- models/connection.js
+++ models/connection.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
 
 //Connet to MongoDB
-mongoose.connect('mongodb+srv://viv1kk:vivek%4015@veggies.epfqdwu.mongodb.net/?retryWrites=true&w=majority', {
 
-mongoose.connect('mongodb://127.0.0.1:27017/Veggies', {
+mongoose.connect('mongodb+srv://viv1kk:vivek%4015@veggies.epfqdwu.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
